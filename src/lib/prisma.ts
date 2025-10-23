01c9784c443de29e824a38e67366767e33491e2e
halej2 <joshua_hale@ymail.com>
2025-10-23 02:19:55 -0500

    Trying to fix db

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
-export default prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
